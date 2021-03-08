0b6e2c6b44a8c961ebe1d2bc3ebe0d600f10a7a5
Saransh <saransh0701@gmail.com>
2021-03-08 18:26:33 +0530

    Tutorial 5"

--- PyBaMM3.py
+++ PyBaMM3.py
@@ -40,7 +40,28 @@ def tut4():
     sim.solve([0, 3600])
     sim.plot()
 
-tut4()
+# tut4()
+
+def tut5():
+
+    experiment = pybamm.Experiment(
+        [
+            "Discharge at C/10 for 10 hours or until 3.3 V",
+            "Rest for 1 hour",
+            "Charge at 1 A until 4.1 V",
+            "Hold at 4.1 V until 50 mA",
+            "Rest for 1 hour",
+        ] * 3
+    )
+
+    model = pybamm.lithium_ion.DFN()
+
+    sim = pybamm.Simulation(model, experiment=experiment)
+
+    sim.solve()
+    sim.plot()
+
+tut5()
 
 # def tweet_graph():
 
