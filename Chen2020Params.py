6ece2635caaa8ef97867d1cf1253f1cf795d5477
Saransh <saransh0701@gmail.com>
2021-03-11 02:39:41 +0530

    Playing with Chen2020 parameters

--- Chen2020Params.py
+++ Chen2020Params.py
@@ -19,7 +19,8 @@ def Chen2020Modelling(current_function, lower_voltage, upper_voltage,
     model = pybamm.lithium_ion.DFN()
     parameter_values = pybamm.ParameterValues(chemistry=chemistry)
     # print(parameter_values)
-    # print(parameter_values.search("temperature"))
+    print(parameter_values.search("Charge"))
+    # model.variables.search("rate")
 
     parameter_values["Current function [A]"] = current_function
     parameter_values["Lower voltage cut-off [V]"] = lower_voltage
@@ -27,10 +28,15 @@ def Chen2020Modelling(current_function, lower_voltage, upper_voltage,
     parameter_values["Ambient temperature [K]"] = ambient_temp
     parameter_values["Initial temperature [K]"] = initial_temp
     parameter_values["Reference temperature [K]"] = reference_temp
+    # parameter_values["C-rate"] = 1
+    # model["C-rate"] = 1
+
+    # output_variables = ["C-rate", "Electrolyte concentration [mol.m-3]", "Terminal voltage [V]"]
 
     sim = pybamm.Simulation(model, parameter_values=parameter_values)
     sim.solve([0, 3600])
     sim.plot()
+    # sim.plot(output_variables=output_variables)
 
 
 def random_plot_generator():
@@ -42,17 +48,17 @@ def random_plot_generator():
         print(upper_voltage)
         lower_voltage = random.randint(0, 10)
         print(lower_voltage)
-        # ambient_temp = random.uniform(273.18, 298.15)
-        # print(ambient_temp)
-        # initial_temp = random.uniform(273.18, 298.15)
-        # print(initial_temp)
-        # reference_temp = random.uniform(273.18, 298.15)
-        # print(reference_temp)
+        ambient_temp = random.uniform(273.18, 298.15)
+        print(ambient_temp)
+        initial_temp = random.uniform(273.18, 298.15)
+        print(initial_temp)
+        reference_temp = random.uniform(273.18, 298.15)
+        print(reference_temp)
         if lower_voltage < upper_voltage:
             Chen2020Modelling(current_function=current_function,
             lower_voltage=lower_voltage, upper_voltage=upper_voltage,
-            ambient_temp=298.15, initial_temp=298.15,
-            reference_temp=298.15)
+            ambient_temp=ambient_temp, initial_temp=initial_temp,
+            reference_temp=reference_temp)
             break
 
 
