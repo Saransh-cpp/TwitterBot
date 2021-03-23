c9f09e6068214e7084ed6b0053bb3548541ee797
Saransh <saransh0701@gmail.com>
2021-03-24 01:40:20 +0530

    Removed random output variables

--- Randomness/PlotGraph.py
+++ Randomness/PlotGraph.py
@@ -8,33 +8,33 @@ def plot_graph(solution, sim, output_variables, t=None, reply=False):
     if t == None:
         t = solution["Time [s]"]
     final_time = int(t.entries[len(t.entries) - 1])
-    plot_type = random.randint(0, 1)
+    # plot_type = random.randint(0, 1)
     time = random.randint(0, final_time)
-    print(time, plot_type)
-    if plot_type == 0:
-        plot = pybamm.QuickPlot(sim, time_unit="seconds")
-        plot.plot(time)
-        if not reply:
-            plot.fig.savefig("foo.png", dpi=300)
-        elif reply:
-            plot.fig.savefig("replyFoo.png", dpi=300)
+    print(time)
+    # if plot_type == 0:
+    plot = pybamm.QuickPlot(sim, time_unit="seconds")
+    plot.plot(time)
+    if not reply:
+        plot.fig.savefig("foo.png", dpi=300)
+    elif reply:
+        plot.fig.savefig("replyFoo.png", dpi=300)
         # foo1.pdf_to_png('foo.pdf')
-    else:
-        while True:
-            lower_limit = random.randint(0, len(output_variables))
-            upper_limit = random.randint(0, len(output_variables))
-            if upper_limit - lower_limit < 9 and upper_limit - lower_limit > 2:
-                plot = pybamm.QuickPlot(
-                    sim,
-                    output_variables=output_variables[lower_limit:upper_limit],
-                    time_unit="seconds",
-                )
-                plot.plot(time)
-                if not reply:
-                    plot.fig.savefig("foo.png", dpi=300)
-                elif reply:
-                    plot.fig.savefig("replyFoo.png", dpi=300)
-                # foo1.pdf_to_png('foo.pdf')
-                break
+    # else:
+    #     while True:
+    #         lower_limit = random.randint(0, len(output_variables))
+    #         upper_limit = random.randint(0, len(output_variables))
+    #         if upper_limit - lower_limit < 9 and upper_limit - lower_limit > 2:
+    #             plot = pybamm.QuickPlot(
+    #                 sim,
+    #                 output_variables=output_variables[lower_limit:upper_limit],
+    #                 time_unit="seconds",
+    #             )
+    #             plot.plot(time)
+    #             if not reply:
+    #                 plot.fig.savefig("foo.png", dpi=300)
+    #             elif reply:
+    #                 plot.fig.savefig("replyFoo.png", dpi=300)
+    #             # foo1.pdf_to_png('foo.pdf')
+    #             break
 
     return time
